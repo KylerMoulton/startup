2127a24eee4ac4497e5bc63267e332b619cef25f
Kyler Moulton <kylermoulton@gmail.com>
2024-03-28 22:04:11 -0600

    create user function to create and add a user to the database

--- database.js
+++ database.js
@@ -25,3 +25,17 @@ const scoreCollection = db.collection('score');
   function getUserByToken(token) {
     return userCollection.findOne({ token: token });
   }
+
+  async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+  }
