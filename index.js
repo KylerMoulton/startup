e212d1fdd4f825d8ad94c39da94e0ed997381b64
Kyler Moulton <kylermoulton@gmail.com>
2024-03-28 21:34:43 -0600

    created a login endpoint

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ apiRouter.post('/auth/create', async (req, res) => {
   }
 });
 
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
 // GetScores
 apiRouter.get('/scores', (_req, res) => {
   res.send(scores);
