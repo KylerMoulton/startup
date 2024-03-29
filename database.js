204554687d67b29eab7ca68b5d9d78142230613e
Kyler Moulton <kylermoulton@gmail.com>
2024-03-28 21:59:49 -0600

    Added function to test connection to the database

--- database.js
+++ database.js
@@ -8,3 +8,12 @@ const client = new MongoClient(url);
 const db = client.db('simon');
 const userCollection = db.collection('user');
 const scoreCollection = db.collection('score');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+    await client.connect();
+    await db.command({ ping: 1 });
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
