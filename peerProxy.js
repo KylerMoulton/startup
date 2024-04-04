9c6b1661e582e5a8a3e0a81d1c8b62e9ac56f184
Kyler Moulton <kylermoulton@gmail.com>
2024-04-03 20:10:04 -0600

    store, handle, close, and respond to connections

--- peerProxy.js
+++ peerProxy.js
@@ -9,4 +9,30 @@ function peerProxy(httpServer) {
         wss.emit('connection', ws, request);
       });
     });
+    let connections = [];
+
+    wss.on('connection', (ws) => {
+      const connection = { id: uuid.v4(), alive: true, ws: ws };
+      connections.push(connection);
+  
+      ws.on('message', function message(data) {
+        connections.forEach((c) => {
+          if (c.id !== connection.id) {
+            c.ws.send(data);
+          }
+        });
+      });
+  
+      ws.on('close', () => {
+        const pos = connections.findIndex((o, i) => o.id === connection.id);
+  
+        if (pos >= 0) {
+          connections.splice(pos, 1);
+        }
+      });
+  
+      ws.on('pong', () => {
+        connection.alive = true;
+      });
+    });
 }
