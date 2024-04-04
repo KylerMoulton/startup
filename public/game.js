431256b471b5a584a799b2eecb6ed40ff546371f
Kyler Moulton <kylermoulton@gmail.com>
2024-04-03 22:18:02 -0600

    fixed issue with not calling app.listen and cleaned up code

--- public/game.js
+++ public/game.js
@@ -273,14 +273,14 @@ document.addEventListener("DOMContentLoaded", function() {
     
     
 
-    // Simulate chat messages that will come over WebSocket
-    setInterval(() => {
-        const score = Math.floor(Math.random() * 3000);
-        const chatText = document.querySelector('.Game-Notifications');
-        chatText.innerHTML =
-        `<div class="player-name"><span class="player-event">Eich</span> scored ${score}</div>` +
-        chatText.innerHTML;
-    }, 5000);
+    // // Simulate chat messages that will come over WebSocket
+    // setInterval(() => {
+    //     const score = Math.floor(Math.random() * 3000);
+    //     const chatText = document.querySelector('.Game-Notifications');
+    //     chatText.innerHTML =
+    //     `<div class="player-name"><span class="player-event">Eich</span> scored ${score}</div>` +
+    //     chatText.innerHTML;
+    // }, 5000);
 
     function configureWebSocket() {
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
