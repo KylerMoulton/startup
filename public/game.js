5e662aeaf3fb662d3f8b01adc59ba869d408e207
Kyler Moulton <kylermoulton@gmail.com>
2024-04-03 20:43:20 -0600

    added functions to send and display messages

--- public/game.js
+++ public/game.js
@@ -277,4 +277,38 @@ document.addEventListener("DOMContentLoaded", function() {
         `<div class="player-name"><span class="player-event">Eich</span> scored ${score}</div>` +
         chatText.innerHTML;
     }, 5000);
+
+    function configureWebSocket() {
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+        this.socket.onopen = (event) => {
+          displayMsg('system', 'game', 'connected');
+        };
+        this.socket.onclose = (event) => {
+          displayMsg('system', 'game', 'disconnected');
+        };
+        this.socket.onmessage = async (event) => {
+          const msg = JSON.parse(await event.data.text());
+          if (msg.type === GameEndEvent) {
+            displayMsg('player', msg.from, `scored ${msg.value.score}`);
+          } else if (msg.type === GameStartEvent) {
+            displayMsg('player', msg.from, `started a new game`);
+          }
+        };
+      }
+    
+      function displayMsg(cls, from, msg) {
+        const chatText = document.querySelector('#player-messages');
+        chatText.innerHTML =
+          `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+      }
+    
+      function broadcastEvent(from, type, value) {
+        const event = {
+          from: from,
+          type: type,
+          value: value,
+        };
+        this.socket.send(JSON.stringify(event));
+      }
 });
